86345be5b4ab591a25ec05e17a989a1c1690a2f3
Josh Darling <joshua.a.darling@gmail.com>
2023-01-07 17:18:52 -0700

    base img urls and sizes added in constants

---
+++ backend/src/constants/imageUrls.js
@@ -0,0 +1,15 @@
+let imageUrls = {
+    baseUrl: "http://image.tmdb.org/t/p/"
+
+}
+
+let urlSizes = {
+    list: "w300",
+    standAlone: "w780"
+}
+
+
+module.exports = {
+    imageUrls,
+    urlSizes
+}
