306a0161de7b8286c017e51c559dfe2a233c710d
Josh Darling <joshua.a.darling@gmail.com>
2023-01-28 20:13:58 -0700

    pretty code reformat for queue.js and queueController.js

--- backend/src/controller/queueController.js
+++ backend/src/controller/queueController.js
@@ -1,51 +1,41 @@
 "use strict";
 class QueueController {
-    constructor(QueueModel, UserModel) {
-      this.QueueModel = QueueModel
-      this.UserModel = UserModel
+  constructor(QueueModel, UserModel) {
+    this.QueueModel = QueueModel;
+    this.UserModel = UserModel;
+  }
+
+  async getQueue(username) {
+    try {
+      const user = new this.UserModel({ username });
+      const getUserId = await user.get();
+      const userId = getUserId.id;
+      const queue = new this.QueueModel(userId);
+      const movieIds = await queue.getQueue();
+      const movies = await queue.getMovies(
+        movieIds.map((id) => id.movieId.toString())
+      );
+      return { movies };
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async getQueue(username){
-        try {
-        const user = new this.UserModel({username})
-        const getUserId = await user.get()    
-        const userId = getUserId.id
-        const queue = new this.QueueModel(userId)
-        const movieIds = await queue.getQueue()
-        //console.log()
-        const movies = await queue.getMovies(movieIds.map((id) => (id.movieId.toString())))
-        return {movies}
-        
-
-            // call queue model
-            
-        } catch (error) {
-            throw error
-            
-        }
+  async addToQueue(movieData, username) {
+    try {
+      const { id, name, poster } = movieData;
+      const user = new this.UserModel({ username });
+      const getuserId = await user.get();
+      const userId = getuserId.id;
+      const queue = new this.QueueModel(userId, id, name, poster);
+      await queue.checkDuplicate();
+      await queue.check();
+      const addedToQueue = await queue.add();
+      return { addedToQueue };
+    } catch (error) {
+      throw error;
     }
-
-    async addToQueue(movieData, username){
-        try {
-            const {id, name, poster} = movieData
-            const user = new this.UserModel({username})
-            const getuserId = await user.get()
-            const userId = getuserId.id
-            const queue = new this.QueueModel(userId, id, name, poster)
-            await queue.checkDuplicate()
-            await queue.check()
-            const addedToQueue = await queue.add()
-            return {addedToQueue}
-            
-           
-            
-        } catch (error) {
-            throw error
-            
-        }
-    }
-
-   
+  }
 }
 
-module.exports = QueueController
+module.exports = QueueController;
--- backend/src/routes/queue.js
+++ backend/src/routes/queue.js
@@ -1,49 +1,34 @@
 "use strict";
 const express = require("express");
-const {ensureCorrectUser} = require("../middleware/auth")
+const { ensureCorrectUser } = require("../middleware/auth");
 
+function queueRoutes({ QueueController, UserModel, QueueModel }) {
+  const router = express.Router();
 
-function queueRoutes({
-    QueueController,
-    UserModel,
-    QueueModel,
+  router.get("/:username", ensureCorrectUser, async function (req, res, next) {
+    try {
+      const username = req.params.username;
+      const queue = new QueueController(QueueModel, UserModel);
+      const data = await queue.getQueue(username);
+      return res.status(200).json(data);
+    } catch (error) {
+      return next(error);
+    }
+  });
 
-}){
-    const router = express.Router();
+  router.post("/:username", ensureCorrectUser, async function (req, res, next) {
+    try {
+      const username = req.params.username;
+      const movieData = req.body;
+      const queue = new QueueController(QueueModel, UserModel);
+      const data = await queue.addToQueue(movieData, username);
+      return res.status(201).json(data);
+    } catch (error) {
+      return next(error);
+    }
+  });
 
-    router.get("/:username", ensureCorrectUser, async function(req, res, next) {
-        try {
-            const username = req.params.username;
-            const queue = new QueueController(QueueModel, UserModel);
-            const data = await queue.getQueue(username)
-            return res.status(200).json(data)
-           
-            
-        } catch (error) {
-            return next(error)
-            
-        }
-
-    })
-
-    router.post("/:username", ensureCorrectUser, async function(req, res, next){
-        try {
-            const username = req.params.username;
-            const movieData = req.body;
-            const queue = new QueueController(QueueModel, UserModel)
-            const data = await queue.addToQueue(movieData, username)
-            return res.status(201).json(data)
-        } catch (error) {
-            return next(error)
-            
-        }
-    })
-
-
-
-    return router
-
-
+  return router;
 }
 
-module.exports.queueRoutes = queueRoutes
+module.exports.queueRoutes = queueRoutes;
