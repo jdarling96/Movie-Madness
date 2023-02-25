b6c3d5e91c291dd77c6d30eb06a1bdde11f28c62
Josh Darling <joshua.a.darling@gmail.com>
2023-02-25 16:21:10 -0700

    /watchlist/:username route setup. Uses TableController and TableModel to complete buisness and db logic.

--- backend/src/routes/watchlist.js
+++ backend/src/routes/watchlist.js
@@ -4,19 +4,45 @@ const express = require("express");
 const { Router } = require("express");
 const { ensureCorrectUser } = require("../middleware/auth");
 
-function watchlistRoutes({ TableController, UserModel, TableModel }){
-    const router = express.Router()
+function watchlistRoutes({ TableController, UserModel, TableModel }) {
+  const router = express.Router();
 
-    router.get("/:username", ensureCorrectUser, async function(req, res, next){
-        try {
-            
-        } catch (error) {
-            
-        }
-    })
+  router.get("/:username", ensureCorrectUser, async function (req, res, next) {
+    try {
+      const username = req.params.username;
+      const watchlist = new TableController(TableModel, UserModel, "watchlist");
+      const data = await watchlist.getTable(username);
+      return res.status(200).json(data);
+    } catch (error) {
+        return next(error)
+    }
+  });
 
+  router.post("/:username", ensureCorrectUser, async function (req, res, next) {
+    try {
+      const username = req.params.username;
+      const movieData = req.body;
+      const watchlist = new TableController(TableModel, UserModel, "watchlist");
+      const addedToWatchlist = await watchlist.addToTable(movieData, username);
+      return res.status(201).json({addedToWatchlist});
+    } catch (error) {
+      return next(error);
+    }
+  });
 
-    return Router
+  router.delete("/:username", ensureCorrectUser, async function (req, res, next) {
+    try {
+      const username = req.params.username
+      const movieData = req.body
+      const watchlist = new TableController(TableModel, UserModel, "watchlist")
+      const deleteFromWatchlist = await watchlist.deleteFromTable(movieData, username)
+      return res.status(200).json({deleteFromWatchlist})
+    } catch (error) {
+      return next(error)
+    }
+  })
+
+  return router;
 }
 
-module.exports.watchlistRoutes = watchlistRoutes
+module.exports.watchlistRoutes = watchlistRoutes;
