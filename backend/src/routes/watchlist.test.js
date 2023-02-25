7ca927a6c0c3131b825b3bf2e5d854482f9ac1d9
Josh Darling <joshua.a.darling@gmail.com>
2023-02-25 16:24:50 -0700

    All HappyPath and UnHappy path tests passed for /watchlist/:username route

---
+++ backend/src/routes/watchlist.test.js
@@ -0,0 +1,120 @@
+"use strict";
+const server = require("../config/server");
+const request = require("supertest");
+const TableController = require("../controller/TableController")
+const TableModel = require("../models/table")
+const UserModel = require("../models/user");
+const AuthServices = require("../services/authServices")
+const {
+  UnauthorizedError,
+  BadRequestError,
+} = require("../expressErrorServices");
+const db = require("../db");
+
+const userToken = (() =>{
+    const token = AuthServices.createToken({username:'TestUser123'})
+    return token
+})();
+
+const testUser = {
+    username : 'TestUser123',
+    password : 'TestUser123',
+    email : 'TestUser123@email.com'
+}
+
+beforeAll(async () => {
+  
+  await db.query("BEGIN");
+  const createTestUser = new UserModel({...testUser})
+  await createTestUser.register()
+});
+
+afterAll(async () => {
+  await db.end();
+});
+
+describe("POST /watchlist/:username => success", () => {
+  let app;
+
+  beforeEach(() => {
+    app = server({
+      TableModel,
+      TableController,
+      UserModel,
+    });
+  });
+
+  test("should return a 201", async () => {
+    console.log(userToken)
+    const res = await request(app).post("/watchlist/TestUser123").send({
+        id: 75,
+        poster: "http://image.tmdb.org/t/p/w185/hll4O5vSAfnZDb6JbnP06GPtz7b.jpg",
+        name: "Mars Attacks!"
+    })
+    .set("authorization", `Bearer ${userToken}`);
+    expect(res.statusCode).toEqual(201);
+    expect(res.body).toEqual({
+        addedToWatchlist:{
+            movieId: 75,
+            userId: expect.any(Number),
+
+        }
+    });
+  });
+});
+
+describe("GET /watchlist/:username => success", () => {
+    let app;
+  
+    beforeEach(() => {
+      app = server({
+        TableModel,
+        TableController,
+        UserModel,
+      });
+    });
+    
+    test("should return a 200", async () => {
+        const res = await request(app).get("/watchlist/TestUser123")
+        .set("authorization", `Bearer ${userToken}`);
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual({
+            movies: [
+                {
+                movie_id: 75,
+                name: "Mars Attacks!",
+                poster_path: "http://image.tmdb.org/t/p/w185/hll4O5vSAfnZDb6JbnP06GPtz7b.jpg",
+
+                }
+                
+
+            ]
+        });
+      });
+  });
+
+  describe("DELETE /watchlist/:username => success", () => {
+    let app;
+  
+    beforeEach(() => {
+      app = server({
+        TableModel,
+        TableController,
+        UserModel,
+      });
+    });
+    afterEach(async () => {
+      await db.query("ROLLBACK");
+    });
+  
+    test("should return a 200", async () => {
+        const res = await request(app).delete("/watchlist/TestUser123").send({
+            id: 75
+        })
+        .set("authorization", `Bearer ${userToken}`);
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual({
+            deleteFromWatchlist: "Movie 75 deleted from watchlist",
+        });
+      });
+  });
